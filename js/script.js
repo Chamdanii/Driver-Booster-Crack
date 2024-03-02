1f19575f55951660067eb6a50aa82c3931d97dee
Chamdanii <rochimkhamdan2001@gmail.com>
2024-03-02 15:07:16 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -13,19 +13,3 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
-
-// // Initialize and add the map
-// function initMap() {
-//   // The location of Geeksforgeeks office
-//   const cafe_office = {
-//     lat: -6.523193009333955,
-//     lng: 110.77535284774221,
-//   };
-
-//   // Create the map, centered at gfg_office
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     // Set the zoom of the map
-//     zoom: 17.56,
-//     center: cafe_office,
-//   });
-// }
